54260b21fde0e99f57b179f329c0db855eaa5b5a
eldar <eldar.zaripoff@gmail.com>
2024-07-02 14:22:24 +0300

    Lesson 16

---
+++ src/test/java/by/mts/BaseTests.java
@@ -0,0 +1,27 @@
+package by.mts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTests {
+    WebDriver chromeDriver;
+
+    @BeforeEach
+    public void setChromeDriver() {
+        System.setProperty("webdriver.chrome.driver", System.getenv("CHROME_DRIVER"));
+        //System.setProperty("webdriver.chrome.driver", "C:\\Temp\\chromedriver-win64\\chromedriver.exe");
+        chromeDriver = new ChromeDriver();
+        chromeDriver.manage().window().maximize();
+        chromeDriver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
+    }
+
+    @AfterEach
+    public void after() {
+        chromeDriver.quit();
+    }
+}
